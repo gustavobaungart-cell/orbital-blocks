238f77c3ea48824519bb3a49c21710e98d214192
Cisco <gustavobaungart@gmail.com>
2026-03-13 21:57:29 -0300

    fix: github pages firebase config + index

---
+++ firebase-config.public.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA-Y9gH1WlknjZisdCPQnBu65rLb-YdXyE",
+  authDomain: "blocks-31618.firebaseapp.com",
+  projectId: "blocks-31618",
+  storageBucket: "blocks-31618.firebasestorage.app",
+  messagingSenderId: "295544036466",
+  appId: "1:295544036466:web:eec54dd77feb359930b6eb"
+};
